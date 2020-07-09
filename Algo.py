41ddcd28cb2784232c8b496894d529fe95674578
Hello-World4 <yajel17@gmail.com>
2020-07-09 21:35:20 +0200

    first algo brut force

--- Algo.py
+++ Algo.py
@@ -1,3 +1,9 @@
-def test():
-    print("test")
-    #stop
+def maxSubarray(arr):
+    max = 0
+    for i in range(len(arr)):
+        sum = 0
+        for j in range(i, len(arr)):
+            sum += arr[j]
+            if sum > max:
+                max = sum
+    return max
