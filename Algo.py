c0aef561bca3bf51c678e77f4e6241828fcbfa57
Hello-World4 <yajel17@gmail.com>
2020-07-09 21:45:27 +0200

    brut force algo

--- Algo.py
+++ Algo.py
@@ -1,9 +1,13 @@
-def maxSubarray(arr):
+def maxSubarray(arr): # complexity : O(n^2)
     max = 0
     for i in range(len(arr)):
         sum = 0
-        for j in range(i, len(arr)):
+        for j in range(i,len(arr)):
             sum += arr[j]
             if sum > max:
                 max = sum
     return max
+
+if __name__ == "__main__":
+    a = [1,-3,2,1,-1]
+    print(maxSubarray(a))
